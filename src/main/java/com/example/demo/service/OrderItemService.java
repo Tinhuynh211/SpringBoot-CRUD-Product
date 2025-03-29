792a3fe0a9fd14a13256f4a896fc0fff2258c2b5
Tinhuynh211 <Tinhuynh211@gmail.com>
2025-03-29 16:46:35 +0700

    [TinHV][ADDTRANSACTIONAL]

--- src/main/java/com/example/demo/service/OrderItemService.java
+++ src/main/java/com/example/demo/service/OrderItemService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.OrderItem;
 import com.example.demo.repository.OrderItemRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,16 @@ public class OrderItemService {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+
+    @Transactional
+    public void saveOrderItem(OrderItem orderItem) {
+        orderItemRepository.save(orderItem);
+    }
+
+    @Transactional
+    public void deleteOrderItem(int orderItemId) {
+        orderItemRepository.deleteById(orderItemId);
+    }
     public List<OrderItem> getOrderItems() {
         return orderItemRepository.findAll();
     }
